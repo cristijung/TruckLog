23c55e9892d0720d5b3d68b934a48b73ad621785
Rafael <rafaelramos0401@gmail.com>
2023-03-31 10:46:32 -0300

    added: charts to dashboard

--- src/pages/Admin/Dashboard/Dashboard.tsx
+++ src/pages/Admin/Dashboard/Dashboard.tsx
@@ -8,6 +8,8 @@ import { CreateUserModal } from "../../../shared/components/User/Modals/UserModa
 import { RemoveUserModal } from "../../../shared/components/User/Modals/UserModals/RemoveUserModal";
 import { AddRoleModal } from "../../../shared/components/User/Modals";
 import { FlagBanner, GasPump, Users, Truck } from "@phosphor-icons/react";
+import { BarChart } from "../../../shared/components/User/BarChart";
+import { PieChart } from "../../../shared/components/User/PieChart";
 
 export const Dashboard = () => {
     const { users } = useUsers();
@@ -69,38 +71,44 @@ export const Dashboard = () => {
                     </div>
                 </div>
 
-                <div className="data-container">
-                    <div className="card-data">
-                        <div className="card-header">
-                            <span>Viagens realizadas até o momento</span>
-                            <FlagBanner size={32} />
+                <div className="charts-data-container">
+                    <div className="data-container">
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Viagens realizadas até o momento</span>
+                                <FlagBanner size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
                         </div>
-                        <strong>2000</strong>
-                        <span>15 nos útimos 7 dias</span>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Postos cadastrados</span>
+                                <GasPump size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Caminhões cadastrados</span>
+                                <Users size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Motoristas cadastrados</span>
+                                <Truck size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
                     </div>
-                    <div className="card-data">
-                        <div className="card-header">
-                            <span>Postos cadastrados</span>
-                            <GasPump size={32} />
-                        </div>
-                        <strong>2000</strong>
-                        <span>15 nos útimos 7 dias</span>
-                    </div>
-                    <div className="card-data">
-                        <div className="card-header">
-                            <span>Caminhões cadastrados</span>
-                            <Users size={32} />
-                        </div>
-                        <strong>2000</strong>
-                        <span>15 nos útimos 7 dias</span>
-                    </div>
-                    <div className="card-data">
-                        <div className="card-header">
-                            <span>Motoristas cadastrados</span>
-                            <Truck size={32} />
-                        </div>
-                        <strong>2000</strong>
-                        <span>15 nos útimos 7 dias</span>
+                    <div className="chart-container">
+                        <BarChart />
+                        <PieChart />
                     </div>
                 </div>
             </main>
--- src/pages/Admin/Dashboard/styles.ts
+++ src/pages/Admin/Dashboard/styles.ts
@@ -16,6 +16,7 @@ export const UsersContainer = styled.main`
     [class$="content"] {
         max-width: 80vw;
         margin: 0 auto;
+        padding: 0 1rem 0 calc(1rem + 20px);
     }
 
     main {
@@ -59,17 +60,26 @@ export const UsersContainer = styled.main`
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding-right: 5rem;
+    }
+
+    .charts-data-container {
+        margin: 2rem auto;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3rem;
     }
 
     .data-container {
-        padding: 3rem 4rem;
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         gap: 3rem;
+        width: 100%;
         .card-data {
             display: flex;
             flex-direction: column;
+            justify-content: ;
             gap: 1rem;
             padding: 1.5rem;
             border-radius: 8px;
@@ -90,6 +100,38 @@ export const UsersContainer = styled.main`
             strong {
                 font-size: 1.8rem;
             }
+        }
+    }
+
+    .chart-container {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 2rem;
+        justify-content: space-between;
+        max-height: 480px;
+        width: 100%;
+
+        canvas {
+            border-radius: 8px;
+            padding: 1rem;
+            background-color: ${(props) => props.theme.background};
+            width: 100%;
+            height: 100%;
+        }
+
+        canvas:first-child {
+            flex-grow: 1;
+        }
+    }
+
+    @media (max-width: 1280px) {
+        .data-container {
+            grid-template-columns: repeat(2, 1fr);
+            justify-content: center;
+        }
+
+        .canvas:last-child {
+            flex-grow: 1;
         }
     }
 `;
