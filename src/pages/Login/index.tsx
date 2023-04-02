46a447d0e0a62a640cc4755c44495642ee08b45c
Luan Lima <limaluan.dev@gmail.com>
2023-04-02 18:04:18 -0300

    hotfix: show error on fail authentication

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -20,8 +20,8 @@ import InterestModal from "../../shared/components/Homepage/InterestModal";
 import ForgotPassModal from "../../shared/components/Homepage/ForgotPassModal";
 
 interface ILoginForm {
-    login: string;
-    senha: string;
+  login: string;
+  senha: string;
 }
 
 export const Login = () => {
@@ -32,7 +32,7 @@ export const Login = () => {
   } = useForm({
     resolver: yupResolver(schemaLogin),
   });
-  
+
   const navigate = useNavigate();
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
 
@@ -43,60 +43,64 @@ export const Login = () => {
   // AUTHENTICATION
   const [_token, setToken] = useState(localStorage.getItem("token"));
   const [authLogin] = useAuthLoginMutation();
-    const handleResize = () => {
-        setIsMobile(window.innerWidth <= 600);
-    };
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 600);
+  };
 
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    const handleLoginSubmit: SubmitHandler<any> = (data: ILoginForm) => {
-        authLogin({
-            login: data.login,
-            senha: data.senha,
-        })
-            .unwrap()
-            .then((response: any) => {
-                localStorage.setItem("token", response);
-                setToken(response);
-                navigate("/usuario/dashboard");
-            })
-            .catch((error: any) => {
-                toast.error(error.message);
-            });
-    };
-    return (
-        <LoginContainer>
-            <div className="back-button">
-                <Link className="link-button" to="/">
-                    {isMobile ? (
-                        <>
-                            <FaHome size={45} />
-                        </>
-                    ) : (
-                        <>
-                            <BsArrowLeft size={35} />
-                            <span>Página Inicial</span>
-                        </>
-                    )}
-                </Link>
-            </div>
-            <div className="bg-items">
-                <img className="bg-object1" src={bgObject1Img} alt="" />
-                <img className="bg-object2" src={bgObject2Img} alt="" />
-                <img
-                    className="bg-truck"
-                    src={truckImg}
-                    alt="imagem de uma caminhão em uma extremidade com a logo do trucklog"
-                />
-            </div>
-            <form onSubmit={handleSubmit(handleLoginSubmit)}>
-                <div className="form-section">
-                    <h1>Login</h1>
-                    <h3>Insira seus dados de acesso:</h3>
+  const handleLoginSubmit: SubmitHandler<any> = (data: ILoginForm) => {
+    authLogin({
+      login: data.login,
+      senha: data.senha,
+    })
+      .unwrap()
+      .then((response: any) => {
+        localStorage.setItem("token", response);
+        setToken(response);
+        navigate("/usuario/dashboard");
+      })
+      .catch((error: any) => {
+        toast.error(`Login ou senha inválido`);
+        document.querySelector(".error")?.classList.add("visible"),
+          document.querySelectorAll(".input-container").forEach((input) => {
+            input.classList.add("outlined-error");
+          });
+      });
+  };
+  return (
+    <LoginContainer>
+      <div className="back-button">
+        <Link className="link-button" to="/">
+          {isMobile ? (
+            <>
+              <FaHome size={45} />
+            </>
+          ) : (
+            <>
+              <BsArrowLeft size={35} />
+              <span>Página Inicial</span>
+            </>
+          )}
+        </Link>
+      </div>
+      <div className="bg-items">
+        <img className="bg-object1" src={bgObject1Img} alt="" />
+        <img className="bg-object2" src={bgObject2Img} alt="" />
+        <img
+          className="bg-truck"
+          src={truckImg}
+          alt="imagem de uma caminhão em uma extremidade com a logo do trucklog"
+        />
+      </div>
+      <form onSubmit={handleSubmit(handleLoginSubmit)}>
+        <div className="form-section">
+          <h1>Login</h1>
+          <h3>Insira seus dados de acesso:</h3>
 
           <div className={`input-container `}>
             <i className="ph ph-envelope"></i>
