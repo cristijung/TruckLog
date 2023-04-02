b7a914b4b85eeca1633acf37a12eaa13c329319b
Rafael <rafaelramos0401@gmail.com>
2023-04-02 10:52:16 -0300

    fixed: dashboard error on login

--- src/pages/Admin/Dashboard/Dashboard.tsx
+++ src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,110 +1,83 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from "react";
 
-import { useUsers } from '../../../shared/hooks/useUsers';
-import { UsersContainer } from './styles';
-import { AuthContext } from '../../../shared/context/AuthContext';
-import { EditUserModal } from '../../../shared/components/User/Modals/UserModals/EditUserModal';
-import { CreateUserModal } from '../../../shared/components/User/Modals/UserModals/CreateUserModal';
-import { RemoveUserModal } from '../../../shared/components/User/Modals/UserModals/RemoveUserModal';
-import { AddRoleModal } from '../../../shared/components/User/Modals';
-import { useGetLoggedUserQuery } from '../../../redux/features/Authentication/authenticationSlice';
-import { FlagBanner, GasPump, Users, Truck } from '@phosphor-icons/react';
-import { BarChart } from '../../../shared/components/User/BarChart';
-import { PieChart } from '../../../shared/components/User/PieChart';
+import { useUsers } from "../../../shared/hooks/useUsers";
+import { UsersContainer } from "./styles";
+import { AuthContext } from "../../../shared/context/AuthContext";
+import { EditUserModal } from "../../../shared/components/User/Modals/UserModals/EditUserModal";
+import { CreateUserModal } from "../../../shared/components/User/Modals/UserModals/CreateUserModal";
+import { RemoveUserModal } from "../../../shared/components/User/Modals/UserModals/RemoveUserModal";
+import { AddRoleModal } from "../../../shared/components/User/Modals";
+import { useGetLoggedUserQuery } from "../../../redux/features/Authentication/authenticationSlice";
+import { FlagBanner, GasPump, Users, Truck } from "@phosphor-icons/react";
+import { BarChart } from "../../../shared/components/User/BarChart";
+import { PieChart } from "../../../shared/components/User/PieChart";
 export const Dashboard = () => {
-  const { users } = useUsers();
-  const { data: loggedUser } = useGetLoggedUserQuery();
-  const [isCreateUserModalOpen, setIsCreateUserModalOpen] = useState(false);
-  const [isEditUserModalOpen, setIsEditUserModalOpen] = useState(false);
-  const [isRemoveUSerModalOpen, setIsRemoveUserModalOpen] = useState(false);
-  const [isAddRoleModalOpen, setIsAddRoleModalOpen] = useState(false);
+    const { data: loggedUser } = useGetLoggedUserQuery();
 
-  const [idUserEdit, setIdUserEdit] = useState(0);
-  const [idUserRemove, setIdUserRemove] = useState(0);
+    useEffect(() => {
+        document.title = "Dashboard | TruckLog";
+    }, []);
 
-  const [userName, setUserName] = useState('');
+    return (
+        <UsersContainer>
+            <main className="content">
+                <div className="user-trail">
+                    <span>Meu Painel</span>
+                    <span>{" > "}</span>
+                    <a className="selected">Dashboard</a>
+                </div>
 
-  const handleAddRole = (idUsuario: number) => {
-    setIsAddRoleModalOpen(true);
-    setIdUserEdit(idUsuario);
-  };
+                <div className="page-header">
+                    <div>
+                        <h2 className="title-page">Olá {loggedUser?.nome}</h2>
+                    </div>
 
-  const handleOpenEditModal = (user: number) => {
-    setIsEditUserModalOpen(true);
-    setIdUserEdit(user);
-  };
+                    <div>
+                        <h2 className="title-page">Visão Geral</h2>
+                    </div>
+                </div>
 
-  const handleRemoveUserModal = (idUsuario: number, name: string) => {
-    setIsRemoveUserModalOpen(true);
-    setUserName(name);
-    setIdUserRemove(idUsuario);
-  };
-
-  useEffect(() => {
-    document.title = 'Dashboard | TruckLog';
-  }, []);
-
-  const [searchUser, setSearchUsers] = useState('');
-  return (
-    <UsersContainer>
-      <main className="content">
-        <div className="user-trail">
-          <span>Meu Painel</span>
-          <span>{' > '}</span>
-          <a className="selected">Dashboard</a>
-        </div>
-
-        <div className="page-header">
-          <div>
-            <h2 className="title-page">Olá {loggedUser?.nome}</h2>
-          </div>
-
-          <div>
-            <h2 className="title-page">Visão Geral</h2>
-          </div>
-        </div>
-
-        <div className="charts-data-container">
-          <div className="data-container">
-            <div className="card-data">
-              <div className="card-header">
-                <span>Viagens realizadas</span>
-                <FlagBanner size={32} />
-              </div>
-              <strong>2000</strong>
-              <span>15 nos útimos 7 dias</span>
-            </div>
-            <div className="card-data">
-              <div className="card-header">
-                <span>Postos cadastrados</span>
-                <GasPump size={32} />
-              </div>
-              <strong>2000</strong>
-              <span>15 nos útimos 7 dias</span>
-            </div>
-            <div className="card-data">
-              <div className="card-header">
-                <span>Caminhões cadastrados</span>
-                <Users size={32} />
-              </div>
-              <strong>2000</strong>
-              <span>15 nos útimos 7 dias</span>
-            </div>
-            <div className="card-data">
-              <div className="card-header">
-                <span>Motoristas cadastrados</span>
-                <Truck size={32} />
-              </div>
-              <strong>2000</strong>
-              <span>15 nos útimos 7 dias</span>
-            </div>
-          </div>
-          <div className="chart-container">
-            <BarChart />
-            <PieChart />
-          </div>
-        </div>
-      </main>
-    </UsersContainer>
-  );
+                <div className="charts-data-container">
+                    <div className="data-container">
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Viagens realizadas</span>
+                                <FlagBanner size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Postos cadastrados</span>
+                                <GasPump size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Caminhões cadastrados</span>
+                                <Users size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                        <div className="card-data">
+                            <div className="card-header">
+                                <span>Motoristas cadastrados</span>
+                                <Truck size={32} />
+                            </div>
+                            <strong>2000</strong>
+                            <span>15 nos útimos 7 dias</span>
+                        </div>
+                    </div>
+                    <div className="chart-container">
+                        <BarChart />
+                        <PieChart />
+                    </div>
+                </div>
+            </main>
+        </UsersContainer>
+    );
 };
